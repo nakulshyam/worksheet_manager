e5c4973f051709c4c0c0f2f782a247c83f8dc3be
Nakul Shyamkumar <lactic.nakul@gmail.com>
2019-10-03 06:34:16 +0000

    disallow

--- app.js
+++ app.js
@@ -206,24 +206,24 @@ app.get('/students', function(req, res) {
 
 // ===========================================
 
-// GET register
-app.get('/register', function(req, res) {
-	res.render('register');
-});
+// // GET register
+// app.get('/register', function(req, res) {
+// 	res.render('register');
+// });
 
-// POST register
-app.post('/register', function(req, res) {
-	var newUser = new Teacher({ username: req.body.username });
-	Teacher.register(newUser, req.body.password, function(err, user) {
-		if (err) {
-			console.log(err);
-			return res.render('register');
-		}
-		passport.authenticate('local')(req, res, function() {
-			res.redirect('/posts');
-		});
-	});
-});
+// // POST register
+// app.post('/register', function(req, res) {
+// 	var newUser = new Teacher({ username: req.body.username });
+// 	Teacher.register(newUser, req.body.password, function(err, user) {
+// 		if (err) {
+// 			console.log(err);
+// 			return res.render('register');
+// 		}
+// 		passport.authenticate('local')(req, res, function() {
+// 			res.redirect('/posts');
+// 		});
+// 	});
+// });
 
 // GET login form
 app.get('/login', function(req, res) {
